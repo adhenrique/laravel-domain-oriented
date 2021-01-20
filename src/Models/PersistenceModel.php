b1a99c8f66cbbefec9695709b533e037acce48ee
adhenrique <adhenriquemusic@gmail.com>
2021-01-20 12:38:08 -0300

    adding HasFactory into parent model

--- src/Models/PersistenceModel.php
+++ src/Models/PersistenceModel.php
@@ -2,12 +2,13 @@
 
 namespace LaravelDomainOriented\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PersistenceModel extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
 
     protected $fillable = [
         'name',
